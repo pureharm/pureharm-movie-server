cbc8c9c1c8f50650749f97b446d333aa1533aea7
Lorand Szakacs <lsz@lorandszakacs.com>
2018-06-20 22:31:40 +0300

    Create module pms-effects

---
+++ pms-effects/src/main/scala/busylabs/effects/package.scala
@@ -0,0 +1,37 @@
+package busylabs
+
+import busymachines.effects.async._
+import busymachines.effects.sync._
+
+import busymachines.effects.sync.validated._
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 20 Jun 2018
+  *
+  */
+package object effects extends AnyRef with OptionSyntax.Implicits with OptionSyntaxAsync.Implcits with TryTypeDefinitons
+  with TrySyntax.Implicits with TrySyntaxAsync.Implcits with EitherSyntax.Implicits with EitherSyntaxAsync.Implcits
+  with ResultTypeDefinitions with ResultCompanionAliases with ResultSyntax.Implicits with ResultSyntaxAsync.Implcits
+  with FutureTypeDefinitions with FutureSyntax.Implicits with IOTypeDefinitions with IOSyntax.Implicits
+  with TaskTypeDefinitions with TaskSyntax.Implicits {
+
+  type NonEmptyList[A] = cats.data.NonEmptyList[A]
+  @inline def NonEmptyList: cats.data.NonEmptyList.type  = cats.data.NonEmptyList
+
+  type Seq[A] = scala.collection.immutable.Seq[A]
+  @inline def Seq:scala.collection.immutable.Seq.type = scala.collection.immutable.Seq
+
+  type Sync[F[_]] = cats.effect.Sync[F]
+  @inline def Sync: cats.effect.Sync.type = cats.effect.Sync
+
+  type Async[F[_]] = cats.effect.Async[F]
+  @inline def Async: cats.effect.Async.type = cats.effect.Async
+
+  type Effect[F[_]] = cats.effect.Effect[F]
+  @inline def Effect: cats.effect.Effect.type = cats.effect.Effect
+
+  object validated extends ValidatedTypeDefinitions with ValidatedSyntax.Implicits with ValidatedSyntaxAsync.Implcits
+
+}
