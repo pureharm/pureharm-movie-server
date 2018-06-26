421012de93cbf4e94e837dd1ce711bc002d21ca5
Lorand Szakacs <lsz@lorandszakacs.com>
2018-06-26 11:27:22 +0300

    Add UserRegistration case class

---
+++ algebra-user/src/main/scala/pms/algebra/user/UserRegistration.scala
@@ -0,0 +1,15 @@
+package pms.algebra.user
+
+import pms.core._
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 26 Jun 2018
+  *
+  */
+final case class UserRegistration(
+  email: Email,
+  pw:    PlainTextPassword,
+  role:  UserRole
+)
