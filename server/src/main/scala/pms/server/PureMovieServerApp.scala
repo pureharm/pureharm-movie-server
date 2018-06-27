820823f584bfec5a649bb59d4555db406dee7b1f
Lorand Szakacs <lsz@lorandszakacs.com>
2018-06-27 17:13:58 +0300

    Wire up server with PureMovieServer services

--- server/src/main/scala/pms/server/PureMovieServerApp.scala
+++ server/src/main/scala/pms/server/PureMovieServerApp.scala
@@ -1,10 +1,11 @@
 package pms.server
 
 import pms.effects._
+import pms.email._
 
 import fs2.{Stream, StreamApp}
-import org.http4s.HttpService
-import org.http4s.server.blaze.BlazeBuilder
+import org.http4s._
+import org.http4s.server.blaze._
 
 /**
   *
@@ -14,22 +15,28 @@ import org.http4s.server.blaze.BlazeBuilder
   */
 object PureMovieServerApp extends StreamApp[IO] {
 
-  private def pureMovieHttpServices[F[_]: Effect]: HttpService[F] =
-    ???
-
-  private def serverStream[F[_]: Effect](
-    config:      PureMovieServerConfig
-  )(implicit ec: ExecutionContext): Stream[F, StreamApp.ExitCode] =
-    BlazeBuilder[F]
-      .bindHttp(config.port, config.host)
-      .mountService(pureMovieHttpServices, config.apiRoot)
-      .serve
-
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, StreamApp.ExitCode] = {
     implicit val sch: Scheduler = Scheduler.global
     for {
-      config   <- Stream.eval(PureMovieServerConfig.default[IO])
-      exitCode <- serverStream[IO](config)
+      serverConfig <- Stream.eval(PureMovieServerConfig.default[IO])
+      gmailConfig  <- Stream.eval(GmailConfig.default[IO])
+      pmsModule    <- Stream.eval(pureMovieServerModule[IO](gmailConfig))
+      exitCode <- serverStream[IO](
+                   config  = serverConfig,
+                   service = pmsModule.pureMovieServerService
+                 )
     } yield exitCode
   }
+
+  private def pureMovieServerModule[F[_]: Concurrent](gmailConfig: GmailConfig): F[ModulePureMovieServer[F]] =
+    Concurrent.apply[F].delay(ModulePureMovieServer.concurrent(gmailConfig))
+
+  private def serverStream[F[_]: Effect: Concurrent](
+    config:      PureMovieServerConfig,
+    service:     HttpService[F]
+  )(implicit ec: ExecutionContext): Stream[F, StreamApp.ExitCode] =
+    BlazeBuilder[F]
+      .bindHttp(config.port, config.host)
+      .mountService(service, config.apiRoot)
+      .serve
 }
