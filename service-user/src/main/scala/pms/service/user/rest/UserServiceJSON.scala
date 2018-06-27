1cb993ae351e66e1de155a03544e8a42352e5070
Lorand Szakacs <lsz@lorandszakacs.com>
2018-06-27 17:26:16 +0300

    Implement JSON codecs

--- service-user/src/main/scala/pms/service/user/rest/UserServiceJSON.scala
+++ service-user/src/main/scala/pms/service/user/rest/UserServiceJSON.scala
@@ -13,19 +13,33 @@ import pms.service.user._
   */
 trait UserServiceJSON extends PMSJson {
 
-  implicit val userRegistrationCirceCodec: Codec[UserRegistration] = ???
+  implicit val userRoleCirceCodec: Codec[UserRole] = Codec.instance(
+    encode = Encoder.apply[String].contramap(ur => ur.productPrefix),
+    decode = Decoder.apply[String].emap(s => UserRole.fromName(s).left.map(_.message))
+  )
 
-  implicit val userCirceCodec: Codec[User] = ???
+  implicit val userIDCirceCodec: Codec[UserID] = Codec.instance[UserID](
+    encode = Encoder.apply[Long].contramap(UserID.exorcise),
+    decode = Decoder.apply[Long].map(UserID.haunt)
+  )
 
-  implicit val pwResetReqCirceCodec: Codec[PasswordResetRequest] =
-    ???
+  implicit val passwordResetTokenCirceCodec: Codec[PasswordResetToken] = Codec.instance[PasswordResetToken](
+    encode = Encoder.apply[String].contramap(PasswordResetToken.exorcise),
+    decode = Decoder.apply[String].map(PasswordResetToken.haunt)
+  )
 
-  implicit val pwResetComCirceCodec: Codec[PasswordResetCompletion] =
-    ???
+  implicit val authenticationTokenCirceCodec: Codec[AuthenticationToken] = Codec.instance(
+    encode = Encoder.apply[String].contramap(AuthenticationToken.exorcise),
+    decode = Decoder.apply[String].map(AuthenticationToken.haunt)
+  )
 
-  implicit val authenticationTokenCirceCodec: Codec[AuthenticationToken] =
-    ???
+  implicit val userRegistrationCirceCodec: Codec[UserRegistration] = derive.codec[UserRegistration]
 
-  implicit val authCtxCirceCodec: Codec[AuthCtx] =
-    ???
+  implicit val userCirceCodec: Codec[User] = derive.codec[User]
+
+  implicit val pwResetReqCirceCodec: Codec[PasswordResetRequest] = derive.codec[PasswordResetRequest]
+
+  implicit val pwResetComCirceCodec: Codec[PasswordResetCompletion] = derive.codec[PasswordResetCompletion]
+
+  implicit val authCtxCirceCodec: Codec[AuthCtx] = derive.codec[AuthCtx]
 }
