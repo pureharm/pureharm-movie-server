3617b4fd7e22d4f31c1533ceeb7c7ab6021ddbb8
Lorand Szakacs <lsz@lorandszakacs.com>
2018-07-13 15:44:35 +0300

    Fix injection of Scheduler into pure movie server

--- server/src/main/scala/pms/server/PureMovieServer.scala
+++ server/src/main/scala/pms/server/PureMovieServer.scala
@@ -1,12 +1,15 @@
 package pms.server
 
 import cats.implicits._
+
 import pms.effects._
 import pms.email._
 import pms.db.config._
+
+import pms.algebra.imdb.IMDBAlgebraConfig
+
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import doobie.util.transactor.Transactor
-import pms.algebra.imdb.IMDBAlgebraConfig
 
 /**
   *
@@ -14,45 +17,49 @@ import pms.algebra.imdb.IMDBAlgebraConfig
   * @since 11 Jul 2018
   *
   */
-final class PureMovieServer[F[_]: Concurrent] private () {
-  private val F: Concurrent[F] = Concurrent.apply[F]
+final class PureMovieServer[F[_]] private (
+  implicit private val F: Concurrent[F],
+  private val scheduler:  Scheduler
+) {
   private val logger = Slf4jLogger.unsafeCreate[F]
 
   def init: F[(PureMovieServerConfig, ModulePureMovieServer[F])] = {
     for {
-      serverConfig <- PureMovieServerConfig.default[F]
-      gmailConfig  <- GmailConfig.default[F]
+      serverConfig      <- PureMovieServerConfig.default[F]
+      gmailConfig       <- GmailConfig.default[F]
       imdbAlgebraConfig <- IMDBAlgebraConfig.default[F]
-      dbConfig     <- DatabaseConfig.default[F]
-      transactor   <- DatabaseConfigAlgebra.transactor[F](dbConfig)
-      nrOfMigs     <- DatabaseConfigAlgebra.initializeSQLDb[F](dbConfig)
-      _            <- logger.info(s"Successfully ran #$nrOfMigs migrations")
-      pmsModule    <- moduleInit(
-        gmailConfig,
-        imdbAlgebraConfig,
-        transactor,
-        bootstrap = serverConfig.bootstrap
-      )
-      _            <- logger.info(s"Successfully initialized pure-movie-server")
+      dbConfig          <- DatabaseConfig.default[F]
+      transactor        <- DatabaseConfigAlgebra.transactor[F](dbConfig)
+      nrOfMigs          <- DatabaseConfigAlgebra.initializeSQLDb[F](dbConfig)
+      _                 <- logger.info(s"Successfully ran #$nrOfMigs migrations")
+      pmsModule <- moduleInit(
+                    gmailConfig,
+                    imdbAlgebraConfig,
+                    bootstrap = serverConfig.bootstrap
+                  )(transactor, implicitly[Scheduler])
+      _ <- logger.info(s"Successfully initialized pure-movie-server")
     } yield (serverConfig, pmsModule)
   }
 
   private def moduleInit(
-    gmailConfig: GmailConfig,
+    gmailConfig:      GmailConfig,
     imdblgebraConfig: IMDBAlgebraConfig,
-    transactor:  Transactor[F],
-    bootstrap:   Boolean
+    bootstrap:        Boolean
+  )(
+    implicit
+    transactor: Transactor[F],
+    scheduler:  Scheduler
   ): F[ModulePureMovieServer[F]] = {
     if (bootstrap) {
       logger.warn(
         "BOOTSTRAP — initializing server in bootstrap mode — if this is on prod, you seriously botched this one"
       ) *>
-        F.delay(ModulePureMovieServerBootstrap.concurrent(gmailConfig)(F, transactor)).flatMap { module =>
+        F.delay(ModulePureMovieServerBootstrap.concurrent(gmailConfig, imdblgebraConfig)).flatMap { module =>
           module.bootstrap >> F.pure(module)
         }
     }
     else {
-      F.delay(ModulePureMovieServer.concurrent(gmailConfig)(F, transactor))
+      F.delay(ModulePureMovieServer.concurrent(gmailConfig, imdblgebraConfig))
     }
 
   }
@@ -60,5 +67,7 @@ final class PureMovieServer[F[_]: Concurrent] private () {
 }
 
 object PureMovieServer {
-  def concurrent[F[_]: Concurrent]: F[PureMovieServer[F]] = Concurrent.apply[F].delay(new PureMovieServer[F]())
+
+  def concurrent[F[_]: Concurrent](implicit sch: Scheduler): F[PureMovieServer[F]] =
+    Concurrent.apply[F].delay(new PureMovieServer[F]())
 }
