b11d36ed148209c31cd3412931488b3907adb13e
Lorand Szakacs <lsz@lorandszakacs.com>
2018-07-15 13:11:52 +0300

    Remove useles pms.http package object
    Government is as unreal, as intangible, as
    unapproachable as God. Try it, if you don't
    believe it. Seek through the legislative halls
    of America and find, if you can, the Government.
    In the end you will be doomed to confer with the
    agent, as before.

--- pms-http/src/main/scala/pms/http/package.scala
+++
@@ -1,9 +0,0 @@
-package pms
-
-/**
-  *
-  * @author Lorand Szakacs, https://github.com/lorandszakacs
-  * @since 26 Jun 2018
-  *
-  */
-package object http extends CirceToHttp4sEncoders with JavaTimeQueryParameterDecoders
