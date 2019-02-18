617313305061024947b96836846aef801953d552
andrei babos <babosandrei@gmail.com>
2019-02-18 17:00:11 +0200

    Add initial implementation  of RateLimiter.throttle

--- algreba-imdb/src/main/scala/pms/algebra/imdb/extra/RateLimiter.scala
+++ algreba-imdb/src/main/scala/pms/algebra/imdb/extra/RateLimiter.scala
@@ -23,44 +23,66 @@ import cats.effect.{Async, Timer}
 final private[imdb] class RateLimiter[F[_]: Timer: Async, T] private (
   val interval:            FiniteDuration,
   val size:                Int,
-  private val initialTime: Ref[F, Long]
+  private val initialTime: Ref[F, FiniteDuration]
 ) {
 
-  def throttle(f: F[T]): F[T] = ???
+  def throttle(f: F[T]): F[T] =
+    delayLogic >> f <* updateTimeRefLogic
 
-//  // might be replaceable with cats.effect.Timer
-//  private val requestQueue = new ConcurrentLinkedQueue[() => Promise[T]]() ///care e faza cu parametrizarea?
-//
-//  /* scheduler.scheduleAtFixedRate(0.millis, interval) {
-//     for (_ <- 0 until size if !requestQueue.isEmpty) {
-//       val request = requestQueue.poll()
-//       request.apply()
-//     }
-//   }*/
-//
-//  Async[F].delay {
-//    Timer.apply.sleep(interval) {
-//      for (_ <- 0 until size if !requestQueue.isEmpty) {
-//        val request = requestQueue.poll()
-//        request.apply()
-//      }
-//    }
-//    //    def example(thunk: => T): F[T] = Async[F].delay(thunk)
-//    //
-//    //    example(throw new RuntimeException("3493-0259"))
-//
-//    def addToQueue(f: => T): Future[T] = {
-//      val promise = Promise[T]() ///declar a promise
-//      requestQueue.add(() => {
-//        promise.completeWith(Future {
-//          f
-//        })
-//      })
-//      //obtain the future that it complete
-//
-//      promise.future
-//    }
-//  }
+  private def delayLogic: F[Unit] =
+    for {
+      init <- initialTime.get
+      now  <- timeNow
+      _ <- if (isWithinInterval(init, now))
+            Timer[F].sleep(init.max(now) - init.min(now))
+          else
+            Async[F].unit
+    } yield ()
+
+  private def updateTimeRefLogic: F[Unit] =
+    for {
+      now <- timeNow
+      _   <- initialTime.set(now)
+    } yield ()
+
+  private def isWithinInterval(init: FiniteDuration, now: FiniteDuration): Boolean =
+    init - now < interval //TODO:
+
+  private def timeNow: F[FiniteDuration] =
+    Timer[F].clock.monotonic(interval.unit).map(l => FiniteDuration(l, interval.unit))
+  //  // might be replaceable with cats.effect.Timer
+  //  private val requestQueue = new ConcurrentLinkedQueue[() => Promise[T]]() ///care e faza cu parametrizarea?
+  //
+  //  /* scheduler.scheduleAtFixedRate(0.millis, interval) {
+  //     for (_ <- 0 until size if !requestQueue.isEmpty) {
+  //       val request = requestQueue.poll()
+  //       request.apply()
+  //     }
+  //   }*/
+  //
+  //  Async[F].delay {
+  //    Timer.apply.sleep(interval) {
+  //      for (_ <- 0 until size if !requestQueue.isEmpty) {
+  //        val request = requestQueue.poll()
+  //        request.apply()
+  //      }
+  //    }
+  //    //    def example(thunk: => T): F[T] = Async[F].delay(thunk)
+  //    //
+  //    //    example(throw new RuntimeException("3493-0259"))
+  //
+  //    def addToQueue(f: => T): Future[T] = {
+  //      val promise = Promise[T]() ///declar a promise
+  //      requestQueue.add(() => {
+  //        promise.completeWith(Future {
+  //          f
+  //        })
+  //      })
+  //      //obtain the future that it complete
+  //
+  //      promise.future
+  //    }
+  //  }
 }
 
 object RateLimiter {
@@ -71,7 +93,8 @@ object RateLimiter {
   ): F[RateLimiter[F, T]] = {
     for {
       initTime <- Timer[F].clock.monotonic(interval.unit)
-      ref      <- Ref.of[F, Long](initTime)
+      ref      <- Ref.of[F, FiniteDuration](FiniteDuration(initTime, interval.unit))
+
     } yield
       new RateLimiter[F, T](
         interval    = interval,
@@ -80,7 +103,7 @@ object RateLimiter {
       )
   }
 
-//  def async[F[_]: Async, T](interval: FiniteDuration, size: Int)(implicit scheduler: Scheduler): RateLimiter[F, T] = {
-//    new RateLimiter[F, T](interval, size)
-//  }
+  //  def async[F[_]: Async, T](interval: FiniteDuration, size: Int)(implicit scheduler: Scheduler): RateLimiter[F, T] = {
+  //    new RateLimiter[F, T](interval, size)
+  //  }
 }
