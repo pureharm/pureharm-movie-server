777ac41c42476d6cf4bb395f9af967c4cd5c8c29
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-20 14:07:05 +0200

    Improve DB schema

--- module/utils/db-config/src/main/resources/db/migration/V0_0_1__Initial_database_setup.sql
+++ module/utils/db-config/src/main/resources/db/migration/V0_0_1__Initial_database_setup.sql
@@ -1,23 +1,38 @@
-CREATE TABLE users (
-  id serial NOT NULL,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  role varchar(255) NOT NULL,
-  registration varchar(255) NOT NULL,
-  passwordReset varchar(255),
-  PRIMARY KEY (id)
+CREATE TYPE user_role AS ENUM(
+    'Newbie',
+    'Member',
+    'Curator',
+    'SuperAdmin'
 );
 
-CREATE TABLE authentications (
-  id serial NOT NULL,
-  userId integer NOT NULL,
-  token varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE users (
+  id uuid PRIMARY KEY NOT NULL,
+  email varchar(255) NOT NULL,
+  password bytea NOT NULL,
+  role user_role NOT NULL,
+  registration_token varchar(255) NOT NULL,
+  password_reset_token varchar(255),
+);
+
+CREATE TABLE user_invitations (
+  email varchar(255) PRIMARY KEY NOT NULL,
+  role user_role NOT NULL,
+  invitation_token varchar(255) NOT NULL,
+  expires_at timestamptz NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_user_invitations_unique_token ON user_invitations (invitation_token);
+
+CREATE TABLE user_authentications (
+  token varchar(255) PRIMARY KEY NOT NULL,
+  user_id uuid NOT NULL,
+  expires_at timestamptz NOT NULL,
+  CONSTRAINT fk_authentications_on_users_id
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE movies (
-  id serial NOT NULL,
+  id uuid PRIMARY KEY NOT NULL,
   name varchar(255) NOT NULL,
-  date timestamp NOT NULL,
-  PRIMARY KEY (id)
+  release_date date NOT NULL
 )
--- module/utils/db-config/src/main/resources/db/migration/V0_0_4__rename_registration_to_invitation.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE user_invitation
-RENAME COLUMN registration TO invitation_token;
-
-DROP INDEX registration_idx;
-CREATE UNIQUE INDEX invitation_token_idx ON user_invitation (invitation_token);
