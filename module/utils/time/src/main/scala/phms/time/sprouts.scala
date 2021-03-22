bdfd0ece78c1aff7db78a794e49631c1dd4c3a76
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-22 14:26:32 +0200

    Add SproutTimestamp.isInPast helper method

--- module/utils/time/src/main/scala/phms/time/sprouts.scala
+++ module/utils/time/src/main/scala/phms/time/sprouts.scala
@@ -5,6 +5,9 @@ import phms._
 trait SproutTimestamp extends Sprout[OffsetDateTime] {
   def now[F[_]](implicit F:      Applicative[F], t: Time[F]): F[Type] = t.now.map(this.newType)
   def tomorrow[F[_]](implicit F: Applicative[F], t: Time[F]): F[Type] = t.now.map(n => this.newType(n.plusDays(1)))
+
+  def isInPast[F[_]](that: Type)(implicit F: Applicative[F], t: Time[F]): F[Boolean] =
+    t.now.map(now => now.isAfter(oldType(that)))
 }
 
 trait SproutSubTimestamp extends SproutSub[OffsetDateTime] {
