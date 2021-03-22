35e044b4a1a869a65d3ac542542e17f69ffaee8a
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-22 19:39:51 +0200

    Add SproutUUID.fromString method

--- module/utils/core/src/main/scala/phms/SproutUUID.scala
+++ module/utils/core/src/main/scala/phms/SproutUUID.scala
@@ -2,6 +2,9 @@ package phms
 
 trait SproutUUID extends Sprout[java.util.UUID] {
 
+  def fromString[F[_]](s: String)(implicit F: ApplicativeThrow[F]): F[Type] =
+    F.catchNonFatal(this.newType(java.util.UUID.fromString(s)))
+
   def generate[F[_]](implicit F: Applicative[F], random: Random[F]): F[Type] =
     random.nextBytes(16).map(b => this.newType(java.util.UUID.nameUUIDFromBytes(b)))
 }
