c43d794969dc8a34c92ae3f74e23c7ece25800ab
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-24 15:44:24 +0200

    Add named loggers

--- modules/utils/logger/src/main/scala/phms/logger/Logging.scala
+++ modules/utils/logger/src/main/scala/phms/logger/Logging.scala
@@ -3,13 +3,15 @@ package phms.logger
 import phms._
 
 trait Logging[F[_]] {
-  def of(a: AnyRef): Logger[F]
+  def of(a:    AnyRef): Logger[F]
+  def named(s: String): Logger[F]
 }
 
 object Logging {
 
   def resource[F[_]: Sync]: Resource[F, Logging[F]] =
     new Logging[F] {
-      override def of(a: AnyRef): Logger[F] = Logger.getLoggerFromName(a.getClass.getCanonicalName.stripSuffix("$"))
+      override def of(a:    AnyRef): Logger[F] = Logger.getLoggerFromName(a.getClass.getCanonicalName.stripSuffix("$"))
+      override def named(s: String): Logger[F] = Logger.getLoggerFromName(s)
     }.pure[Resource[F, *]]
 }
