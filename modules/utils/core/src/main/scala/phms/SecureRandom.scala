f30a7fe14e44483d7cdf5dd27ac9beed6918215f
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-27 10:15:55 +0200

    Compile core w/ scala 3

--- modules/utils/core/src/main/scala/phms/SecureRandom.scala
+++ modules/utils/core/src/main/scala/phms/SecureRandom.scala
@@ -26,7 +26,7 @@ object SecureRandom {
 
   def resource[F[_]](implicit F: Sync[F]): Resource[F, SecureRandom[F]] =
     Resource.eval[F, SecureRandom[F]] {
-      cats.effect.std.Random.javaSecuritySecureRandom(8).map { r: Random[F] =>
+      cats.effect.std.Random.javaSecuritySecureRandom(8).map { (r: Random[F]) =>
         new SecureRandom[F] {
           override def sync: Sync[F] = F
           override def nextBytes(n:         Int): F[Array[Byte]] = r.nextBytes(n)
