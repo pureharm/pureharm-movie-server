7f73f0817895037bea8c608e6c6155345401badf
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-04-12 13:24:59 +0300

    Replace postman collection w/ insomnia collection

--- modules/apps/bootstrap/src/main/scala/phms/server/bootstrap/Bootstrap.scala
+++ modules/apps/bootstrap/src/main/scala/phms/server/bootstrap/Bootstrap.scala
@@ -27,12 +27,12 @@ import phms.logger.Logging
 object Bootstrap {
 
   object superAdmin {
-    def email[F[_]: MonadThrow]: F[Email]             = Email[F]("murray.bookchin@socialecology.fecund")
+    def email[F[_]: MonadThrow]: F[Email]             = Email[F]("murray.bookchin@social.ecology")
     def passw[F[_]: MonadThrow]: F[PlainTextPassword] = PlainTextPassword[F]("OldManYellsAtAnarchism")
 
     //the concatenated base64($email:$passw) string, on hand, in case needed
     val BasicAuthEncoding: String =
-      "bXVycmF5LmJvb2tjaGluQHNvY2lhbGVjb2xvZ3kuZmVjdW5kOk9sZE1hblllbGxzQXRBbmFyY2hpc20="
+      "bXVycmF5LmJvb2tjaGluQHNvY2lhbC5lY29sb2d5Ok9sZE1hblllbGxzQXRBbmFyY2hpc20K"
   }
 
   def bootstrap[F[_]](
